e52a66262a857d9d18fda3fb3189e78d72b09a6c
hsingpu <1927833039@qq.com>
2019-05-25 18:14:32 +0800

    socket网络编程

--- 008socket基础/socketlist.py
+++ 008socket基础/socketlist.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import socket
+# import socket
 import socketserver
 
 
--- day05/code/day05/jd.py
+++
@@ -1,7 +0,0 @@
-
-def func():
-    print("我是jd里面的一个函数")
-
-name = "alex"
-age = 33
-
