c6238ea5d02da37791352743bc05d74310039dab
hsingpu <1927833039@qq.com>
2019-07-13 17:22:16 +0800

    django

---
+++ 014/mysite2/app01/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+
+# Create your views here.
+
+
+def login(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        pwd=request.POST.get('pwd')
+        if name=='hp' and pwd=='123':
+            # return redirect('index')
+            return render(request,'index.html',{'name':name})
+
+    return render(request,'login.html')
+
+def index(request):
+    return render(request,'index.html')
