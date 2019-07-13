81592fc8ee75a67acf1501cbf57c407268d4cc37
5StevenWu <1927833039@qq.com>
2019-07-13 17:44:31 +0800

    index

--- 014/mysite2/app01/views.py
+++ 014/mysite2/app01/views.py
@@ -8,8 +8,8 @@ def login(request):
         name=request.POST.get('name')
         pwd=request.POST.get('pwd')
         if name=='hp' and pwd=='123':
-            # return redirect('index')
-            return render(request,'index.html',{'name':name})
+            return redirect('/index/')
+            # return render(request,'index.html',{'name':name})
 
     return render(request,'login.html')
 
